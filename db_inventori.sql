048f0b454680b0e7cfa34182902075029bc0d20d
satrio <satriogaming1@gmail.com>
2024-01-16 17:30:51 +0700

    add database

--- db_inventori.sql
+++ db_inventori.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 26, 2023 at 05:04 AM
--- Server version: 10.4.20-MariaDB
--- PHP Version: 7.3.29
+-- Generation Time: Jan 16, 2024 at 11:29 AM
+-- Server version: 10.4.28-MariaDB-log
+-- PHP Version: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `inventori`
+-- Database: `db_inventori`
 --
 
 -- --------------------------------------------------------
@@ -31,17 +31,45 @@ CREATE TABLE `barang` (
   `id` int(11) NOT NULL,
   `kode_barang` varchar(20) DEFAULT NULL,
   `nama_barang` varchar(80) DEFAULT NULL,
+  `harga` int(11) NOT NULL,
+  `deskripsi` longtext NOT NULL,
+  `deskripsi2` varchar(255) NOT NULL,
+  `deskripsi3` varchar(255) NOT NULL,
+  `deskripsi4` varchar(255) NOT NULL,
+  `deskripsi5` varchar(255) NOT NULL,
+  `deskripsi6` varchar(255) NOT NULL,
   `stok` int(11) DEFAULT NULL,
-  `satuan` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `gambar` blob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `barang`
 --
 
-INSERT INTO `barang` (`id`, `kode_barang`, `nama_barang`, `stok`, `satuan`) VALUES
-(1, '36143613', 'Keyboard', 12, 'pcs'),
-(2, '15747218', 'Mouse', 6, 'pcs');
+INSERT INTO `barang` (`id`, `kode_barang`, `nama_barang`, `harga`, `deskripsi`, `deskripsi2`, `deskripsi3`, `deskripsi4`, `deskripsi5`, `deskripsi6`, `stok`, `gambar`) VALUES
+(46, 'KD_214', 'Silver Pacakagee', 250000, 'All Soft Files', '10 edited photos', '2 backgroundd', '30 minutes photo session', '1 printed out photo (10R sized)', 'All soft files', NULL, 0x53637265656e73686f745f323032342d30312d31305f3031333033382e706e67),
+(47, 'KD_931', 'Gold Pacakage', 300000, '10 photo poses', '10 edited photos', '2 background', '30 minutes photo session', '1 printed out photo (10R sized)', 'All soft files', NULL, 0x53637265656e73686f745f323032342d30312d31305f3031333230362e706e67),
+(48, 'KD_218', 'Graduation Pacakage', 250000, 'Unlimited Foto', '25 Photoshoot', 'Edit 6 foto', 'All foto send via Gdrive', 'Max 6 person', '2 printed out photo (10R sized)', NULL, 0x53637265656e73686f745f323032342d30312d31305f3031333335302e706e67),
+(49, 'KD_989', 'Preweding Pacakage', 400000, '10 photo poses', '10 edited photos', '2 background', '100 minutes photo session', '1 printed out photo (10R sized)', 'All soft files', NULL, 0x53637265656e73686f745f323032342d30312d31305f3031333533332e706e67);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `gambar` blob NOT NULL,
+  `selected_date` varchar(30) NOT NULL,
+  `selected_time` varchar(30) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -50,20 +78,27 @@ INSERT INTO `barang` (`id`, `kode_barang`, `nama_barang`, `stok`, `satuan`) VALU
 --
 
 CREATE TABLE `customer` (
-  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `kode` varchar(20) DEFAULT NULL,
   `nama` varchar(80) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(25) NOT NULL,
   `telepon` varchar(15) DEFAULT NULL,
   `alamat` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `customer`
 --
 
-INSERT INTO `customer` (`id`, `kode`, `nama`, `email`, `telepon`, `alamat`) VALUES
-(1, 'CST240', 'Smart Computer', 'smtcm@web.com', '082146738976', 'Sidareja');
+INSERT INTO `customer` (`user_id`, `kode`, `nama`, `username`, `password`, `telepon`, `alamat`) VALUES
+(2, 'CST782', 'budi', 'budi@gmail.com', 'budi', '123123123', 'kudus'),
+(4, 'CST205', 'adit tidak pakai koma', 'adit1@gmail.com', 'adit', '214124', 'parti'),
+(9, 'CST272', 'fiant tob', 'a@gmail.com', 'a', '231412421', 'a'),
+(10, 'CST496', 'b', 'b@gmamil.com', 'b', '412412', 'a'),
+(11, 'CST621', 'bagos', 'h@gmail.com', 'h', '1', 'a'),
+(12, 'CST777', 'Rudi', 'ru@gmail.com', 'r', '12312312', 'kds'),
+(13, 'CST_123', 'eri', 'eri@gmail.com', '123', '324234', 'kudus');
 
 -- --------------------------------------------------------
 
@@ -77,14 +112,14 @@ CREATE TABLE `data_toko` (
   `nama_pemilik` varchar(80) DEFAULT NULL,
   `no_telepon` varchar(15) DEFAULT NULL,
   `alamat` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `data_toko`
 --
 
 INSERT INTO `data_toko` (`id`, `nama_toko`, `nama_pemilik`, `no_telepon`, `alamat`) VALUES
-(1, 'Toko Maju Jaya', 'Nugroho', '081299764535', 'Sidareja');
+(1, 'Popo - Lulu Studio', 'Akhles', '081299764535', 'Kudus');
 
 -- --------------------------------------------------------
 
@@ -96,16 +131,18 @@ CREATE TABLE `detail_keluar` (
   `no_keluar` varchar(25) DEFAULT NULL,
   `nama_barang` varchar(80) DEFAULT NULL,
   `jumlah` int(11) DEFAULT NULL,
+  `harga` int(25) NOT NULL,
   `satuan` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `detail_keluar`
 --
 
-INSERT INTO `detail_keluar` (`no_keluar`, `nama_barang`, `jumlah`, `satuan`) VALUES
-('TR1584538942', 'Keyboard', 1, 'pcs'),
-('TR1584538942', 'Mouse', 1, 'pcs');
+INSERT INTO `detail_keluar` (`no_keluar`, `nama_barang`, `jumlah`, `harga`, `satuan`) VALUES
+('TR1703351639', 'Sony A7IV', 1, 300000, NULL),
+('TR1703351718', 'Sony A7IV', 1, 300000, NULL),
+('TR1703351844', 'Fuji-film Xt3', 2, 250000, NULL);
 
 -- --------------------------------------------------------
 
@@ -118,7 +155,7 @@ CREATE TABLE `detail_terima` (
   `nama_barang` varchar(80) DEFAULT NULL,
   `jumlah` int(11) DEFAULT NULL,
   `satuan` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `detail_terima`
@@ -128,6 +165,50 @@ INSERT INTO `detail_terima` (`no_terima`, `nama_barang`, `jumlah`, `satuan`) VAL
 ('TR1584538872', 'Keyboard', 1, 'pcs'),
 ('TR1584538872', 'Mouse', 1, 'pcs'),
 ('TR1584539271', 'Keyboard', 4, 'pcs');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `order_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status` varchar(30) NOT NULL,
+  `total_amount` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`order_id`, `user_id`, `order_date`, `status`, `total_amount`) VALUES
+(74, 9, '2024-01-16 03:49:00', 'diproses', 0.00);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_items`
+--
+
+CREATE TABLE `order_items` (
+  `id` int(11) NOT NULL,
+  `order_id` int(11) DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `selected_date` varchar(30) DEFAULT NULL,
+  `selected_time` varchar(30) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `order_items`
+--
+
+INSERT INTO `order_items` (`id`, `order_id`, `product_id`, `selected_date`, `selected_time`, `quantity`, `price`) VALUES
+(47, 74, 47, '2024-01-17', '19:50', 1, 300000.00);
 
 -- --------------------------------------------------------
 
@@ -142,7 +223,7 @@ CREATE TABLE `penerimaan` (
   `jam_terima` varchar(10) DEFAULT NULL,
   `nama_supplier` varchar(80) DEFAULT NULL,
   `nama_petugas` varchar(80) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `penerimaan`
@@ -165,14 +246,16 @@ CREATE TABLE `pengeluaran` (
   `jam_keluar` varchar(10) DEFAULT NULL,
   `nama_customer` varchar(80) DEFAULT NULL,
   `nama_petugas` varchar(80) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `pengeluaran`
 --
 
 INSERT INTO `pengeluaran` (`id`, `no_keluar`, `tgl_keluar`, `jam_keluar`, `nama_customer`, `nama_petugas`) VALUES
-(5, 'TR1584538942', '18/03/2020', '20:42:22', 'Smart Computer', 'Nugrohoo');
+(7, 'TR1703351639', '24/12/2023', '00:13:59', 'fiant tob', 'Satrio'),
+(8, 'TR1703351718', '24/12/2023', '00:15:18', 'budi', 'Agus'),
+(9, 'TR1703351844', '24/12/2023', '00:17:24', 'adit pakai koma', 'Satrio');
 
 -- --------------------------------------------------------
 
@@ -186,14 +269,14 @@ CREATE TABLE `pengguna` (
   `nama` varchar(100) DEFAULT NULL,
   `username` varchar(20) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `pengguna`
 --
 
 INSERT INTO `pengguna` (`id`, `kode`, `nama`, `username`, `password`) VALUES
-(1, 'PGN17', 'Nugrohoo', 'nugrohoff', 'pwd_nugroho');
+(1, 'ADM', 'Satrio', 'satrio', 'satrio');
 
 -- --------------------------------------------------------
 
@@ -207,13 +290,14 @@ CREATE TABLE `petugas` (
   `nama` varchar(100) DEFAULT NULL,
   `username` varchar(20) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `petugas`
 --
 
 INSERT INTO `petugas` (`id`, `kode`, `nama`, `username`, `password`) VALUES
+(1, 'PETUGAS-1', 'Agus', 'petugas', 'petugas'),
 (3, 'PETUGAS - 35', 'Fanani', 'PTGS35', 'pwd_fanani');
 
 -- --------------------------------------------------------
@@ -229,7 +313,7 @@ CREATE TABLE `supplier` (
   `email` varchar(100) DEFAULT NULL,
   `telepon` varchar(15) DEFAULT NULL,
   `alamat` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `supplier`
@@ -249,15 +333,34 @@ ALTER TABLE `barang`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `customer`
 --
 ALTER TABLE `customer`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`user_id`);
 
 --
 -- Indexes for table `data_toko`
 --
 ALTER TABLE `data_toko`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `order_items`
+--
+ALTER TABLE `order_items`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -300,19 +403,37 @@ ALTER TABLE `supplier`
 -- AUTO_INCREMENT for table `barang`
 --
 ALTER TABLE `barang`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=187;
 
 --
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `data_toko`
 --
 ALTER TABLE `data_toko`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=75;
+
+--
+-- AUTO_INCREMENT for table `order_items`
+--
+ALTER TABLE `order_items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
 -- AUTO_INCREMENT for table `penerimaan`
@@ -324,7 +445,7 @@ ALTER TABLE `penerimaan`
 -- AUTO_INCREMENT for table `pengeluaran`
 --
 ALTER TABLE `pengeluaran`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `pengguna`
@@ -343,6 +464,16 @@ ALTER TABLE `petugas`
 --
 ALTER TABLE `supplier`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `customer` (`user_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
